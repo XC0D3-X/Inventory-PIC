2d99f524982ffed8147879026ae498c700c2ac02
Sh1n <147030291+88sh1n@users.noreply.github.com>
2024-01-10 23:24:05 +0800

    Makmal-Komputer3.php

--- Makmal-Komputer3.php
+++ Makmal-Komputer3.php
@@ -1,10 +1,18 @@
 <?php 
 include_once('layouts/header.php'); 
+include_once("search.php");
 include_once("config.php");
 
-$result = mysqli_query($mysqli, "SELECT * FROM mk3 ORDER BY item_id");
+$tableName = 'mk3'; // Default table name
+$search = isset($_GET['search']) ? $_GET['search'] : '';
+if (isset($_GET['table'])) {
+    $tableName = $_GET['table'];
+}
+$entries = isset($_GET['entries']) ? $_GET['entries'] : 10;
 
+$result = performSearch($tableName, $search, $entries, $mysqli);
 ?>
+
 <a href="javascript:history.go(-1)" class="btn btn-primary btn-lg glyphicon glyphicon-arrow-left"></a>
 <div class="col-md-12 col-12">
     <h1>List Item</h1>
@@ -20,32 +28,49 @@ $result = mysqli_query($mysqli, "SELECT * FROM mk3 ORDER BY item_id");
                 </strong>
             </div>
             <div class="panel-body">
-                <table class="table table-bordered table-striped">
-                    <thead>
-                        <tr>
-                            <th class="text-center" style="width: 50px;">No</th>
-                            <th> Hardware Name </th>
-                            <th class="text-center" style="width: 15%;"> Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        while($res = mysqli_fetch_array($result)) {
-                            echo "<tr>";  
-                            echo "<td>".$res['item_id']."</td>"; 
-                            echo "<td>".$res['item_name']."</td>";
-                            echo "<td>".$res['status']."</td>";
-                            // $fileNames = explode(",", $res['nama']); // Assuming files are comma-separated
-                            // echo "<td>";
-                            // foreach ($fileNames as $fileName) {
-                            //     $fileLocation = "view/" . $fileName;
-                            //     echo date("F d Y H:i:s.", filemtime($fileLocation)) . "<br>";
-                            // };
-                            echo "</tr>";
-                        }
-                        ?>
-                    </tbody>
-                </table>
+                <form method="get" action="">
+                    <label for="entries">Show:</label>
+                    <select id="entries" name="entries" onchange="this.form.submit()">
+                        <option value="10" <?php if($entries == '10') echo 'selected'; ?>>10 entries</option>
+                        <option value="25" <?php if($entries == '25') echo 'selected'; ?>>25 entries</option>
+                        <option value="50" <?php if($entries == '50') echo 'selected'; ?>>50 entries</option>
+                        <option value="100" <?php if($entries == '100') echo 'selected'; ?>>100 entries</option>
+                    </select>
+                </form>
+                
+                <form method="get" action="">
+                    <label for="search">Search:</label>
+                    <input type="text" id="search" name="search" placeholder="Enter search keyword">
+                    <input type="submit" value="Search">
+                </form>
+
+                <?php if (mysqli_num_rows($result) > 0): ?>
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th class="text-center" style="width: 50px;">No</th>
+                                <th> Hardware Name </th>action
+                                <th class="text-center" style="width: 15%;"> Status</th>
+                                <th class="text-center" style="width: 15%;"> Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            while($res = mysqli_fetch_array($result)) {
+                                echo "<tr>";  
+                                echo "<td>".$res['item_id']."</td>"; 
+                                echo "<td>".$res['item_name']."</td>";
+                                echo "<td>".$res['status']."</td>";
+                                echo "<td>","<center><Update  href=\"edit.php?item_id=".$res['item_id']."\"class=\"btn btn-primary btn-md\">Update</center></td>
+                                ";
+                                echo "</tr>";
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                <?php else: ?>
+                    <p>No results found.</p>
+                <?php endif; ?>
             </div>
         </div>
     </div>
